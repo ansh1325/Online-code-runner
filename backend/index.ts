e60555c299f6445f06b6a8e0b0279c7fe1a2104a
ansh1325 <anshtiwari1325@gmail.com>
2026-07-02 08:29:23 +0530

    Did some code

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello via Bun!");
+import express from "express";
+import redis,{createClient} from "redis";
+
+
+const app=express();
+const client=createClient();
+client.connect();
+
+
+app.use(express.json());
+
+app.post("/submission", async(req,res)=>{
+    const userId=req.body.userId;
+    const question=req.body.question;
+    const  code=req.body.code;
+    const language=req.body.language;
+
+    client.lPush("problems", JSON.stringify({userId,question,code,language}));
+
+    res.json({
+        message:"Processing",
+        
+    })
+
+})
+
+app.get("/submission/:submissionId", async(req,res)=>{
+
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
